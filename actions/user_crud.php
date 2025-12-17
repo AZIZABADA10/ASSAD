83f0ee4cfcafea428ceee52f97f80c77cbcfc37b
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-17 23:26:50 +0000

    Correction de bugs dans la requête SQL

--- actions/user_crud.php
+++ actions/user_crud.php
@@ -2,7 +2,7 @@
 
     require_once __DIR__ .'/../config/db.php';
 
-    $users = $connexion -> query("SELECT * FROM utilisateurs GROUP BY `role`");
+    $users = $connexion -> query("SELECT * FROM utilisateurs");
 
 
 ?>
