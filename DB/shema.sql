c63e5b9adc7d92fb4356828cadbb4f33ab569270
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-17 17:06:06 +0000

    fix: modification sur la logique des role sur action authenftification

--- DB/shema.sql
+++ DB/shema.sql
@@ -17,4 +17,4 @@ insert into utilisateurs (nom_complet,email,`role`,mot_de_passe)
 values ('administrateur','admin@assad.ma','admin','Assad@286');
 
 ALTER TABLE utilisateurs 
-MODIFY COLUMN statut_de_compet ENUM('active', 'blocked', 'en_attend') DEFAULT 'en_attend';
+MODIFY COLUMN statut_de_compet ENUM('active', 'blocked', 'en_attend');
