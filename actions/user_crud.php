b476d1f32b109560b5a3985059e1ccda18d9e248
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-17 23:29:30 +0000

    Correction de bugs dans la requête SQL

--- actions/user_crud.php
+++ actions/user_crud.php
@@ -2,7 +2,7 @@
 
     require_once __DIR__ .'/../config/db.php';
 
-    $users = $connexion -> query("SELECT * FROM utilisateurs");
+    $users = $connexion -> query("SELECT * FROM utilisateurs order by id_utilisateur desc");
 
 
 ?>
