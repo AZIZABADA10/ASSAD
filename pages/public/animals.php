6de0cc507b593fab7688333b33e171888b956ca4
AZIZABADA10 <abadaaziz288@gmail.com>
2025-12-20 21:53:39 +0000

    feat: organisation du page animal et la configiration du connexion avec la base de donne

--- pages/public/animals.php
+++ pages/public/animals.php
@@ -1,11 +1,20 @@
 <?php
 
-require_once '../config/db.php';
+require_once '../../config/db.php';
+
+$animal = $connexion -> query("SELECT * FROM animal")->fetch_assoc();
+
+
+
  /** Header  */
 require_once '../layouts/header.php';
+?>
 
-/** main content */
+<main class="mt-80 h-[400px]">
+<?php var_dump($animal) ?>
+</main>
 
-
+<?php
 /** Footer */
 require_once '../layouts/footer.php';
+?>
